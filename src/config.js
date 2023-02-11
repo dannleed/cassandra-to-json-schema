f7c90d04fbccf880e1bdd4f144f0a66643a29002
dannleed <59970198+dannleed@users.noreply.github.com>
2023-02-12 01:46:37 +0200

    add db connection and data seed

--- src/config.js
+++ src/config.js
@@ -1,15 +1,18 @@
-import 'dotenv';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const {
   CASSANDRA_DB_HOST,
   CASSANDRA_DB_PORT,
   CASSANDRA_DB_USER,
   CASSANDRA_DB_PASS,
+  CASSANDRA_DB_LOCAL_DATA_CENTER,
 } = process.env;
 
 export const config = {
-  host: CASSANDRA_DB_HOST,
-  port: CASSANDRA_DB_PORT,
-  user: CASSANDRA_DB_USER,
-  port: CASSANDRA_DB_PASS,
+  host: String(CASSANDRA_DB_HOST),
+  port: Number(CASSANDRA_DB_PORT),
+  user: String(CASSANDRA_DB_USER),
+  password: String(CASSANDRA_DB_PASS),
+  localDataCenter: String(CASSANDRA_DB_LOCAL_DATA_CENTER),
 };
---
+++ src/db/seed.js
@@ -0,0 +1,58 @@
+import { client } from './connection.js';
+
+async function createKeyspace() {
+  await client.execute(`
+    CREATE KEYSPACE IF NOT EXISTS hackolade 
+    WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': '1'}
+  `);
+}
+
+async function useKeyspace() {
+  await client.execute(`
+    USE hackolade
+  `);
+}
+
+async function createTable() {
+  await client.execute(`
+    CREATE TABLE IF NOT EXISTS users (
+      id uuid PRIMARY KEY,
+      name text,
+      surname text,
+      age int,
+      address text
+    );
+  `);
+}
+
+async function insertData() {
+  const query =
+    'INSERT INTO users (id, name, surname, age, address) VALUES (?, ?, ?, ?, ?) IF NOT EXISTS';
+  const params = [
+    '4acabb43-4f5e-46f2-8371-84668da81ad6',
+    'John',
+    'Doe',
+    35,
+    '{"city":"Lviv","street":"Chornovola","house":59}',
+  ];
+  await client.execute(query, params, { prepare: true });
+  const result = await client.execute('SELECT * FROM users;');
+
+  console.table(result.rows);
+  console.log('Data inserted successfully');
+  process.exit(0);
+}
+
+async function run() {
+  try {
+    await createKeyspace();
+    await useKeyspace();
+    await createTable();
+    await insertData();
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+run();
